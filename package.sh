cf37807bf3dac56d8f486ade620da5ab17bddbfe
Philippe Coval <rzr@users.sf.net>
2020-05-30 09:29:54 +0200

    adapter: Add packaging files for moziot
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: I32c6db02ce158d1c578f5a185a931ad98eeadb21

---
+++ package.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+# -*- mode: Bash; tab-width: 2; indent-tabs-mode: t; -*-
+# SPDX-License-Indentifier: MIT
+# Copyright: Philippe Coval <https://purl.org/rzr/>
+set -x
+package="awox-mesh-light-adapter"
+version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
+date=$(git log -1 --date=short --pretty=format:%cd 2>/dev/null \
+           || date -u -r manifest.json)
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
+# Clean up from previous releases
+rm -rf *.tgz package SHA256SUMS lib
+
+# Prep new package
+mkdir package lib
+
+# Pull down Python dependencies
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
+
+# Put package together
+cp -r \
+	 adapter \
+	 lib LICENSE *.json *.py *.sh README.* \
+	 package/
+find package -type f -name '*.pyc' -delete
+find package -type d -empty -delete
+
+# Generate checksums
+cd package
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
+TARFILE="${package}-${version}${TARFILE_SUFFIX}.tgz"
+GZIP="-n" tar czf "${TARFILE}" --mtime="${date}" package
+
+shasum --algorithm 256 "${TARFILE}" > "${TARFILE}.sha256sum"
+
+rm -rf SHA256SUMS package
