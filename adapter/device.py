1be1adf59e1a4b4cb984910d44fbf5e94bad879a
Philippe Coval <rzr@users.sf.net>
2020-06-02 20:08:44 +0200

    adapter: Refactor using control
    I noticed that controller API is doing more than it claim,
    so presets are now actions.
    
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: I64270f5862378b5da06ac6df465853d0a4d17250

--- adapter/device.py
+++ adapter/device.py
@@ -3,12 +3,15 @@
 # Copyright: Phil Coval <https://purl.org/rzr>
 """AwoxMeshLight adapter for Mozilla WebThings Gateway."""
 
+import bluepy
 import os
 import random
+import time
 
 from gateway_addon import Action, Device, Property
 
 _DEBUG = bool(os.getenv('DEBUG')) or False
+
 
 class AwoxMeshLightDevice(Device):
     """AwoxMeshLight device type."""
@@ -41,25 +44,6 @@ class AwoxMeshLightDevice(Device):
         self._type = ['OnOffSwitch', 'MultiLevelSwitch', 'Light', 'ColorControl']
 
         try:
-            self.properties['preset'] = AwoxMeshLightProperty(
-                self,
-                "preset",
-                {
-                    '@type': 'NumberProperty',
-                    'label': "Preset",
-                    'type': 'integer',
-                    'enum': [0, # Red...
-                             1, # Magenta...
-                             2, # Yellow...
-                             3, # Green...
-                             4, # Blue...
-                             5, # Blue (solid)
-                             6  # Purple (solid)
-                    ]
-                },
-                6)
-            self.controller.setPreset(6);
-
             self.properties['on'] = AwoxMeshLightProperty(
                 self,
                 "on",
@@ -118,6 +102,26 @@ class AwoxMeshLightDevice(Device):
                     'title': 'Random Color',
                 }
             )
+            self.add_action(
+                'preset',
+                {
+                    'title': 'Fading preset',
+                    'input': {
+                        'type': 'object',
+                        'required': [
+                            'preset',
+                        ],
+                        'properties': {
+                            'preset': {
+                                'type': 'integer',                                
+                                'default': 0,
+                                'maximum': 6,
+                                'minimum': 0
+                            }
+                        }
+                    }
+                }
+            )
             self.pairing = True
 
         except Exception as ex:
@@ -126,23 +130,24 @@ class AwoxMeshLightDevice(Device):
     def perform_action(self, action):
         if _DEBUG:
             print(action.__dict__)
-        try:
-            if action.name == 'random':
-                color = "#%06x" % random.randint(0, 0xFFFFFF)
-                self.set_property('color', color)
-            elif action.name == 'cold':
-                self.set_property('color', "#ffffff")
-                self.set_property('brightness', 100)
-                self.set_property('on', True)
-                self.controller.setWhite(0x00, 0x7F)
-            elif action.name == 'warm':
-                self.set_property('color', "#ffffff")
-                self.set_property('brightness', 100)
-                self.set_property('on', True)
-                self.controller.setWhite(0x73, 0x63)
-        except Exception:
-            if _DEBUG:
-                print("error: Failed to set_property")
+        if action.name == 'random':
+            color = "#%06x" % random.randint(0, 0xFFFFFF)
+            self.set_property('color', color)
+        elif action.name == 'cold':
+            self.set_property('color', '#ffffff')
+            self.set_property('brightness', 100)
+            self.set_property('on', True)
+            self.controller.setWhite(0x00, 0x7F)
+        elif action.name == 'warm':
+            self.set_property('color', '#ffffff')
+            self.set_property('brightness', 100)
+            self.set_property('on', True)
+            self.controller.setWhite(0x73, 0x63)
+        elif action.name == 'preset':
+            self.set_property('color', '#ffffff')
+            self.set_property('brightness', 100)
+            self.set_property('on', True)
+            self.controller.setPreset(action.input[action.name])
 
     @staticmethod
     def hex_to_rgb(text):
@@ -151,6 +156,40 @@ class AwoxMeshLightDevice(Device):
                  "green": int(text[3:5], 0x10),
                  "blue": int(text[5:7], 0x10)}
         return color
+
+    def control(self, name, value):
+        try:
+            if name == 'on':
+                if (value):
+                    self.controller.on()
+                else:
+                    self.controller.off()
+            elif name == 'brightness':
+                brightness = int(0x0A + ((0x64 - 0x0A) *  value / 100.))
+                if _DEBUG:
+                    print("brightness: 0x%02x" % brightness)
+                    self.controller.setColorBrightness(brightness)
+            elif name == 'color':
+                color = AwoxMeshLightDevice.hex_to_rgb(value)
+                self.controller.setColor(**color)
+        except:
+            if _DEBUG:
+                print("error: Exception in control: ")
+            self.reset()
+
+    def reset(self):
+        if _DEBUG:
+            print("info: reset")
+        try:
+            self.controller.disconnect()
+        finally:
+            time.sleep(2)            
+        try:
+            self.controller.connect()
+        finally:
+            print("info: connected: model=%s" % self.controller.getModelNumber())
+
+
 
 class AwoxMeshLightProperty(Property):
     """Handle changes with controller """
@@ -171,22 +210,21 @@ class AwoxMeshLightProperty(Property):
                   + " from " + str(self.value) + " to " + str(value))
         if value == self.value:
             return
-        try:
-            if self.name == 'on':
-                if bool(value):
-                    self.device.controller.on()
-                else:
-                    self.device.controller.off()
-            elif self.name == 'brightness':
-                self.device.controller.setColorBrightness(int(value))
-            elif self.name == 'color':
-                color = AwoxMeshLightDevice.hex_to_rgb(value)
-                self.device.controller.setColor(**color)
-            elif self.name == 'preset':
-                self.device.controller.setPreset(value)
+        if self.name == 'on':
+            if bool(value):
+                self.device.control('color',
+                                    self.device.properties['color'].value)
+                self.device.control('brightness',
+                                    self.device.properties['brightness'].value)
+            self.device.control(self.name, value)
+        elif self.name == 'brightness':
+            if self.device.properties['on'].value:
+                self.device.control('color',
+                                    self.device.properties['color'].value)
+                self.device.control(self.name, value)
+        elif self.name == 'color':
+            if self.device.properties['on'].value:
+                self.device.control(self.name, value)
 
-            self.set_cached_value(value)
-            self.device.notify_property_changed(self)
-        except Exception:
-            if _DEBUG:
-                print("error: Failed to set_property")
+        self.set_cached_value(value)
+        self.device.notify_property_changed(self)
