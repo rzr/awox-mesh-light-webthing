fcc550f55dbc6e8bf3bfd30d76a59ba4534208c6
Philippe Coval <rzr@users.sf.net>
2020-10-17 09:54:41 +0200

    Release 0.0.8
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: Ied18f0356ef1eb8b55808ed659f0bb1f91afe26e

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/rzr/awox-mesh-light-webthing/'
 
 setup(
     name='awox-mesh-light-webthing',
-    version='0.0.7',
+    version='0.0.8',
     description='Awox Mesh Light WebThing',
     long_description=long_description,
     url=URL,
